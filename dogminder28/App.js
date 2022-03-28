58ae14ce226c72673ce92f20e4b038f14c9099e7
afjal-h <nahidhussain687@gmail.com>
2022-03-28 21:30:21 +0100

    hello world

--- dogminder28/App.js
+++ dogminder28/App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+
 
 export default function App() {
+  const handlepress=() => console.log("Text has been pressed LFG");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.tinyLogo}
+      
+       source={require("./assets/icon.png")}/>
+      <Text onPress={handlepress}>Username</Text>
+      <Text>Password</Text>
+
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#beeede',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  tinyLogo:{
+    width: 200,
+    height: 200,
+    borderRadius:60
+
+
+  }
+  
 });
