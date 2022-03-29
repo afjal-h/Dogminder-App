e5ecbe6a7a874ec5f6503b66235be1c98cd49e5e
afjal-h <nahidhussain687@gmail.com>
2022-03-29 12:45:23 +0100

    signup page update

--- dogminder28/Screens/SignUpScreen.js
+++ dogminder28/Screens/SignUpScreen.js
@@ -1,12 +1,23 @@
-import { View, Text, Alert, TouchableOpacity, Button, Image, StyleSheet } from "react-native";
+import { View, Text, Alert, TouchableOpacity, Button, Image, StyleSheet, TextInput } from "react-native";
 
 
 function SignUpScreen(props) {
     return (
         <View style={styles.container}>
             <Image style={styles.icon} source={require('../assets/icon.png')} />
-            <Text>Username</Text>
-            <Text>Password</Text>
+            <TextInput style={styles.inputBox}
+                placeholder="Email"
+                placeholderTextColor={"#fff"} />
+            <TextInput style={styles.inputBox}
+                placeholder="Password"
+                placeholderTextColor={"#fff"} />
+
+
+
+            <TouchableOpacity style={styles.LoginButton}>
+                <Text style={styles.ButtonText}>Sign Up</Text>
+            </TouchableOpacity>
+
 
         </View>
     );
@@ -19,17 +30,43 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#beeede",
         alignItems: "center",
-        justifyContent: "flex-end",
+        justifyContent: "center",
+
 
     }
 
     ,
+    LoginButton: {
+        width: 100,
+        top: 30,
+        borderRadius: 25,
+        backgroundColor: "#bdbdbd",
+        paddingVertical: 10,
+
+        marginVertical: 10
+
+    },
+
+    ButtonText: {
+        textAlign: "center",
+
+    },
+
+    inputBox: {
+        width: 300,
+        height: 40,
+        top: 30,
+        borderRadius: 25,
+        backgroundColor: "#bdbdbd",
+        paddingHorizontal: 16,
+        marginVertical: 10
+
+    },
     icon: {
         height: 200,
         width: 200,
-        position: "absolute",
         borderRadius: 60,
-        top: 250
+
     },
 
 
