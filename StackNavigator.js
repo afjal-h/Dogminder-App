e6fa875f3226ecf6cf3fb1448438c13ec18d8525
Afjal Hussain <>
2022-04-04 14:14:24 +0100

    added a nav bar to the homescreen

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,29 +1,66 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+//screens
 import WelcomeScreen from './Screens/WelcomeScreen'
 import SignUpScreen from './Screens/SignUpScreen'
-import LoginScreen from './Screens/LoginScreen'
+import HomeScreen from './Screens/HomeScreen'
 import SearchScreen from './Screens/SearchScreen'
 import BookScreen from './Screens/BookScreen'
-import HomeScreen from './Screens/HomeScreen'
 const Stack = createNativeStackNavigator();
-const StackNavigator = () => {
+function WelcomeStackScreen(){
   return (
+
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Group>
         <Stack.Screen name="WelcomePage" component={WelcomeScreen} />
         <Stack.Screen name="SignUpPage" component={SignUpScreen} />
-        <Stack.Screen name="LoginPage" component={LoginScreen} />
-        <Stack.Screen name="SearchPage" component={SearchScreen} />
-        <Stack.Screen name="BookPage" component={BookScreen} />
-        <Stack.Screen name="HomePage" component={HomeScreen}/>
-
-      </Stack.Group>
+        <Stack.Screen name="HomePage" component={TabNav}/>
     </Stack.Navigator>
   )
 }
+function HomeStackScreen(){
+  return(
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="HomePage" component={HomeScreen} />
+  </Stack.Navigator>
+  )
 
-export default StackNavigator
+  
+}
+
+function SearchStackScreen(){
+  return(
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="SearchPage" component={SearchScreen} />
+  </Stack.Navigator>
+  )
+  
+}
+function BookStackScreen(){
+
+  return(
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="BookPage" component={BookScreen} />
+  </Stack.Navigator>
+  )
+
+  
+}
+const Tab= createBottomTabNavigator();
+
+function TabNav(){
+  return(
+  <Tab.Navigator>
+  <Tab.Screen name="Home" component={HomeScreen}/>
+  <Tab.Screen name="Search" component={SearchScreen}/>
+  <Tab.Screen name="Book" component={BookScreen}/>
+</Tab.Navigator>
+  )
+
+}
+
+export default WelcomeStackScreen
 
 const styles = StyleSheet.create({})
